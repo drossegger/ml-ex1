bbe16f6d0b4a65f1013005aa411a06f6156b0c4e
smortezapoor <soroosh.mortezapoor@gmail.com>
2013-11-10 21:58:08 +0100

    UDPATE - Reformatting output

--- routines/resultprocess.py
+++ routines/resultprocess.py
@@ -5,9 +5,9 @@ def printResult(result, label):
 	prediction=result[1]
 	diff=[float(a)-float(b) for a,b in zip(prediction,testlabel)]
 	diffmean=np.mean(diff)
-	print label + ':'
-	print testlabel
-	print prediction
-	print diff
-	print diffmean
-	
+	print label + ':' + '\r\n'
+	for i in range(0, len(testlabel)):
+		print '{0:10}\r\n	{1:10} ==> {2:10}\r\n	{3:10} ==> {4:10}'.format(testlabel[i],'Prediction', prediction[i], 'Diff', diff[i])
+		
+	print ' ------------------------\r\n|{0:10} :: {1:10f}|\r\n ------------------------'.format('Diff Mean', diffmean)
+	print '----------------------------------------------------'
